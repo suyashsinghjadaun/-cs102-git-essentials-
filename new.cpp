3259e7649d291b5ebeead03ffb71b1877c61195d
suyash <suyashjadaun@gmail.com>
2022-05-13 08:18:17 +0530

    Create new.cpp

---
+++ new.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+   ios::sync_with_stdio(0);
+cin.tie(0);
+   int n;
+   long b;
+   char w;
+   float q;
+   double v;
+   signed int p;
+   unsigned int l;
+   long int j;
+   short int k;
+   
+   cin>>p>>l>>j>>k;
+     cout<<p<<l<<j<<k;
+    return 0;  }
